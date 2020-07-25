ae410f15ce589c07d0a696c15cf74ff6649da721
Nicolas Jorge DL <nicolasjorgedl@hotmail.com>
2020-07-25 20:12:10 -0300

    Added all pieaces forms and make they move down

--- game.js
+++ game.js
@@ -12,9 +12,6 @@ const COR_VAZIA = "gray";
 //Variavel que controla o Campo guardando as posições de todas as peças
 let campo = iniciaCampo();
 
-//Constante que tem todos os formatos possiveis das peças do jogo
-const FORMATOS_DE_PEÇAS = [FORMATO_Z, FORMATO_S]
-
 const FORMATO_Z = [[
     [1, 1, 0],
     [0, 1, 1],
@@ -59,16 +56,189 @@ const FORMATO_S = [[
 ]
 ];
 
+const FORMATO_J = [[
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0]
+],
+[
+    [1, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0]
+],
+[
+    [0, 1, 1],
+    [0, 1, 0],
+    [0, 1, 0]
+],
+[
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 0, 1]
+]
+];
 
-function Peça(formato, cor) {
+const FORMATO_L = [[
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 1]
+],
+[
+    [0, 0, 0],
+    [1, 1, 1],
+    [1, 0, 0]
+],
+[
+    [1, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0]
+],
+[
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0]
+]
+];
+
+const FORMATO_T = [[
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0]
+],
+[
+    [0, 1, 0],
+    [1, 1, 0],
+    [0, 1, 0]
+],
+[
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0]
+],
+[
+    [0, 1, 0],
+    [0, 1, 1],
+    [0, 1, 0]
+]
+];
+
+const FORMATO_I = [[
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0]
+],
+[
+    [0, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+],
+[
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 0]
+],
+[
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0]
+]
+];
+
+const FORMATO_O = [[
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0]
+],
+[
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0]
+],
+[
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0]
+],
+[
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0]
+]
+];
+
+
+//Constante que tem todos os formatos possiveis das peças do jogo
+const PECAS = [
+    [FORMATO_Z, "blue"],
+    [FORMATO_S, "green"],
+    [FORMATO_J, "red"],
+    [FORMATO_L, "yellow"],
+    [FORMATO_T, "purple"],
+    [FORMATO_I, "pink"],
+    [FORMATO_O, "orange"],
+];
+
+
+function Peca(formato, cor) {
     this.cor = cor;
     this.formato = formato;
-    this.posição = 0;
-    this.posiçãoAtual = this.formato[this.posição];
+    this.posicao = 0; // isto indica que a peca comeca na direcao padrao
+    this.posicaoAtual = this.formato[this.posicao];
+    this.x = 3;
+    this.y = 2;
+}
+// Funcao que desenha a peca na tela
+Peca.prototype.desenha = function () {
+    for (l = 0; l < this.posicaoAtual.length; l++) {
+        for (c = 0; c < this.posicaoAtual.length; c++) {
+            if (this.posicaoAtual[l][c]) {
+                desenhaQuadrado(this.x + c, this.y + l, this.cor);
+            }
+        }
+    }
+}
+
+//funcao que apaga a peca na tela
+Peca.prototype.apaga = function () {
+    for (l = 0; l < this.posicaoAtual.length; l++) {
+        for (c = 0; c < this.posicaoAtual.length; c++) {
+            if (this.posicaoAtual[l][c]) {
+                desenhaQuadrado(this.x + c, this.y + l, COR_VAZIA);
+            }
+        }
+    }
 }
 
 
+// funçao que move a peca para baixo
+Peca.prototype.moverParaBaixo = function () {
+    this.apaga();
+    this.y++;
+    this.desenha();
+}
 
+
+let p = new Peca(PECAS[0][0], PECAS[0][1])
+
+let tempoInicial = Date.now();
+function cair() {
+    let tempoAtual = Date.now();
+    let delta = tempoAtual - tempoInicial;
+
+    if (delta > 1000) {
+        p.moverParaBaixo();
+        tempoInicial = Date.now();
+    }
+    requestAnimationFrame(cair);
+}
 
 /* 
     Descrição:Função inicia o Campo Vazio e retorna uma matriz de cores
@@ -106,3 +276,7 @@ function desenhaQuadrado(x, y, cor) {
     ctx.strokeStyle = "black";
     ctx.strokeRect(x * TQ, y * TQ, TQ, TQ);
 }
+
+desenhaCampo();
+p.desenha();
+cair();
